1842b1a669537e6097fac2cd2b3b3d9fb3b0b223
bschwyn <ben.schwyn@gmail.com>
2022-07-20 19:36:30 -0700

    Access urls via FastApi, returns json

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
-import re
+import datetime
 import json
+import re
 
 from bs4 import BeautifulSoup
 from fastapi import FastAPI
@@ -9,19 +8,6 @@ from urllib.request import urlopen, Request
 from fuzzywuzzy import fuzz
 
 
-app = FastAPI()
-
-@app.get("/medium")
-async def root():
-    url = 'https://medium.com/swlh/high-performers-dont-quit-jobs-they-quietly-quit-these-things-e4eff96d4c51'
-
-    req = Request(url, headers = {'User-Agent': 'XYZ/3.0'})
-    webpage = urlopen(req, timeout=10)
-    #return webpage
-    html = webpage
-    print(html.read())
-    soup = BeautifulSoup(html.read(), 'html.parser')
-    return soup
 
 def get_links():
     # tag list from https://medium.com/feedium/list-of-medium-top-writer-tags-number-of-hits-and-amount-of-writers-34ce3f4234a6
@@ -92,8 +78,8 @@ def get_links():
                     if r.get('data-action-source'): # odd how '... in r' doesn't work
                         if r['data-action-source'] == 'preview-listing':
                             article_links.append(r['data-action-value'])
-        except:
-            continue
+        except Exception as e:
+            print(e)
 
         if len(article_links) > 100:
             break
@@ -106,10 +92,9 @@ def get_links():
 
 
 
-def scrape():
+def scrape(url):
     #url = 'https://medium.com/swlh/high-performers-dont-quit-jobs-they-quietly-quit-these-things-e4eff96d4c51'
-    url = "https://medium.com/@anas_ali/gpt-3-a-deep-learning-model-for-natural-language-406afde92733?source=tag_archive---------3-----------------------"    #
-
+    #url = "https://medium.com/@anas_ali/gpt-3-a-deep-learning-model-for-natural-language-406afde92733?source=tag_archive---------3-----------------------"    #
 
     # first is sign in
     # second is my user agent while incognito
@@ -124,35 +109,51 @@ def scrape():
     page = urlopen(req, timeout=10)
     soup = BeautifulSoup(page, 'html.parser')
 
-    #text
-    body_text = ""
-    text_boxes = list(soup.body.article.find_all("p"))
-    for t in text_boxes:
-        body_text += " "
-        body_text += t.get_text()
+    try:
+        #text
+        body_text = ""
+        text_boxes = list(soup.body.article.find_all("p"))
+        for t in text_boxes:
+            body_text += " "
+            body_text += t.get_text()
+    except Exception as e:
+           print(e)
 
-    #headers
-    h1_headers = [h.get_text() for h in soup.body.find_all('h1')]
-    h2_headers = [h.get_text() for h in soup.body.find_all('h2')]
-    h3_headers = [h.get_text() for h in soup.body.find_all('h3')]
-    h4_headers = [h.get_text() for h in soup.body.find_all('h4')]
-    h5_headers = [h.get_text() for h in soup.body.find_all('h5')]
-    h6_headers = [h.get_text() for h in soup.body.find_all('h6')]
+    try:
+        #headers
+        h1_headers = [h.get_text() for h in soup.body.find_all('h1')]
+        h2_headers = [h.get_text() for h in soup.body.find_all('h2')]
+        h3_headers = [h.get_text() for h in soup.body.find_all('h3')]
+        h4_headers = [h.get_text() for h in soup.body.find_all('h4')]
+        h5_headers = [h.get_text() for h in soup.body.find_all('h5')]
+        h6_headers = [h.get_text() for h in soup.body.find_all('h6')]
+    except Exception as e:
+        print(e)
 
-    content = str(soup.find(property="article:author")).split(" ")[1]
-    author_page = content.split('"')[1]
-    author = author_page.split('/')[-1][1:]
+    try:
+        content = str(soup.find(property="article:author")).split(" ")[1]
+        author_page = content.split('"')[1]
+        author = author_page.split('/')[-1][1:].split('.')[0]
+    except Exception as e:
+        print(e)
 
-    # yeah yeah DRY whatever
-    content = str(soup.find(property="article:published_time")).split(" ")[1]
-    published_time = content.split('"')[1]
+    try:
+        # yeah yeah DRY whatever
+        content = str(soup.find(property="article:published_time")).split(" ")[1]
+        published_time = content.split('"')[1]
+        accessed_time = datetime.datetime.utcnow().isoformat() + "Z"
 
+    except Exception as e:
+        print(e)
 
-    # title,  formatted like "My Title | By author lastname | Medium"
-    title = soup.head.title.get_text()
-    title_text = title.split("|")[0].strip()
-    # covers special case if '|' char in title (which is allowed),
-    # title_text = "".join(title.split("|")[:-2])
+    try:
+        # title,  formatted like "My Title | By author lastname | Medium"
+        title = soup.head.title.get_text()
+        title_text = title.split("|")[0].strip()
+        # covers special case if '|' char in title (which is allowed),
+        # title_text = "".join(title.split("|")[:-2])
+    except Exception as e:
+        print(e)
 
     # claps and responses
     claps = None
@@ -162,25 +163,28 @@ def scrape():
     # If I can get detailed div tags then try following:
     # https://hackernoon.com/how-to-scrape-a-medium-publication-a-python-tutorial-for-beginners-o8u3t69
 
-    #parse "window.__APOLLO_STATE__", a script where a bunch of data is stored
-    apollo_window = "window.__APOLLO_STATE__ = "
-    pattern = re.compile("^" + apollo_window)
-    apollo = soup.body.find("script", text=pattern).string
-    if apollo.startswith(apollo_window):
-        apollo = apollo[len(apollo_window):]
-        x = json.loads(apollo)
+    try:
+        #parse "window.__APOLLO_STATE__", a script where a bunch of data is stored
+        apollo_window = "window.__APOLLO_STATE__ = "
+        pattern = re.compile("^" + apollo_window)
+        apollo = soup.body.find("script", text=pattern).string
+        if apollo.startswith(apollo_window):
+            apollo = apollo[len(apollo_window):]
+            x = json.loads(apollo)
 
-        # get posts from script where title matches
-        posts = [x[p] for p in x.keys() if p.startswith("Post")]
-        # fuzzy match because of annoying character
-        posts = [p for p in posts if fuzz.ratio(title, p["title"]) > 0.9]
-        if posts: # because of the fuzzy match, this could be totally wrong
+            # get posts from script where title matches
+            posts = [x[p] for p in x.keys() if p.startswith("Post")]
+            # fuzzy match because of annoying character
+            posts = [p for p in posts if fuzz.ratio(title, p["title"]) > 0.9]
+            if posts: # because of the fuzzy match, this could be totally wrong
 
-            # with this method clapCount is parsed as an int, but some articles have Claps as something like "2.1K"
-            # and maybe it's stored as string, and I would need to convert to 2100
-            claps = posts[0]["clapCount"]
-            responses = posts[0]["postResponses"]["count"]
-            reading_time = posts[0]["readingTime"]
+                # with this method clapCount is parsed as an int, but some articles have Claps as something like "2.1K"
+                # and maybe it's stored as string, and I would need to convert to 2100
+                claps = posts[0]["clapCount"]
+                responses = posts[0]["postResponses"]["count"]
+                reading_time = posts[0]["readingTime"]
+    except Exception as e:
+        print(e)
 
     data_model = {
         "author_page": author_page,
@@ -188,7 +192,7 @@ def scrape():
         "article_title": title_text,
         "images": None,
         "date_published": published_time,
-        "date_accessed": None,
+        "date_accessed": accessed_time,
         "reading_time": reading_time,
         "claps": claps,
         "responses": responses,
@@ -204,7 +208,18 @@ def scrape():
     # other things which can be added:
         # shares on facebook, linkedin, twitter, etc.
 
-    print(data_model)
+    return data_model
+    # print(data_model)
+
+
+app = FastAPI()
+
+@app.get("/medium/")
+async def root(url):
+    # example url: http://127.0.0.1:8000/medium/?url=https://medium.com/..."
+    data_model = scrape(url)
+    json_object = json.dumps(data_model)
+    return json_object
 
 if __name__ == '__main__':
     get_links()
